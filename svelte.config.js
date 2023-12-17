63acf77839b9dfd004f04dd804493066bdf1fdcf
zhihengzhanga@gmail.com <zhihengzhanga@gmail.com>
2023-12-17 15:10:04 -0700

    added packages

--- svelte.config.js
+++ svelte.config.js
@@ -2,7 +2,16 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import adapter from '@sveltejs/adapter-auto';
 
 /** @type {import('@sveltejs/kit').Config} */
-
+const package_path = {
+	'@nodes/': 'src/playground/nodes/',
+	'@plugins/': 'src/playground/plugins/',
+	'shared/': 'src/utils/',
+	'@shared/': 'src/shared/',
+	'@lexical/react/': 'src/lib/',
+	'@theme/': 'src/themes/',
+	'@ui/': 'src/playground/ui/',
+	react: 'src/react.svelte'
+};
 export default {
 	// Consult https://svelte.dev/docs#compile-time-svelte-preprocess
 	// for more information about preprocessors
@@ -13,14 +22,14 @@ export default {
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
-		alias: {
-			'@nodes/': 'src/playground/nodes/',
-			'@plugins/': 'src/playground/plugins/',
-			'shared/': 'src/utils/',
-			'@shared/': 'src/shared/',
-			'@lexical/react/': 'src/lib/',
-			'@theme/': 'src/themes/',
-			'@ui/': 'src/playground/ui/',
+		alias: package_path || {
+			'@nodes/*': 'src/playground/nodes/*',
+			'@plugins/*': 'src/playground/plugins/*',
+			'shared/*': 'src/utils/*',
+			'@shared/*': 'src/shared/*',
+			'@lexical/react/*': 'src/lib/*',
+			'@theme/*': 'src/themes/*',
+			'@ui/*': 'src/playground/ui/*',
 			react: 'src/react.svelte'
 		},
 		typescript: {
