59251e046c2a46affb28d6921c4a155589c6cb41
zhihengzhanga@gmail.com <zhihengzhanga@gmail.com>
2024-03-12 20:59:10 -0700

    fix

---
+++ src/Lazy.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	//import { type Settings } from 'lexical-svelte-runes/client';
+	import { mount, onMount } from 'svelte';
+
+	let { ...props } = $props();
+
+	let EditorInstance: any = $state.frozen();
+	onMount(async () => {
+		EditorInstance = (await import('./App.svelte')).default;
+		//	mount(EditorInstance, { target: document.body, props: { config: { editable: false } } });
+	});
+</script>
+
+{#if !EditorInstance}
+	loading
+{/if}
+
+<svelte:component
+	this={EditorInstance}
+	isAutocomplete={false}
+	dev={false}
+	config={{ editable: false }}
+	{...props}
+/>
---
+++ src/index.svelte
@@ -0,0 +1,12 @@
+<script>
+	import Lazy from './Lazy.svelte';
+	let setting = $state({ isAutocomplete: true, config: { editable: true } });
+</script>
+
+<button
+	on:click={() => {
+		setting.isAutocomplete = !setting.isAutocomplete;
+		setting.config.editable = !setting.config.editable;
+	}}>toggle</button
+>
+<Lazy {...setting} />
