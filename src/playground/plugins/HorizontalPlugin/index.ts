39663c126f20c1096c7356cf73c3172a32671647
zhihengzhanga@gmail.com <zhihengzhanga@gmail.com>
2024-03-19 15:57:45 -0700

    fix page break

--- src/playground/plugins/HorizontalPlugin/index.ts
+++ src/playground/plugins/HorizontalPlugin/index.ts
@@ -1,6 +1,5 @@
 import { createCommand, type LexicalCommand } from 'lexical';
 
-export * from './HorizontalRuleComponent.svelte';
 export const INSERT_HORIZONTAL_RULE_COMMAND: LexicalCommand<void> = createCommand(
 	'INSERT_HORIZONTAL_RULE_COMMAND'
 );
