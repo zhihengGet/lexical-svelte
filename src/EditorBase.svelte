d37a30ffe3b538563149c3efd4828ca532b50f3e
zhihengzhanga@gmail.com <zhihengzhanga@gmail.com>
2024-04-27 13:43:10 -0700

    fix imports

---
+++ src/EditorBase.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import Editor from './Editor.svelte';
+	import LexicalComposer from './lib/LexicalComposer.svelte';
+	import SettingsContext from './playground/context/SettingsContext.svelte';
+	import SharedAutocompleteContext from './playground/context/SharedAutocompleteContext.svelte';
+	import { createHistoryContext } from './playground/context/SharedHistoryContext';
+	import type { Settings } from './playground/appSettings';
+	let { ...props }: Settings = $props();
+
+	createHistoryContext();
+</script>
+
+<SettingsContext settings={props}>
+	<SharedAutocompleteContext>
+		<LexicalComposer>
+			<div class="editor-shell w-full">
+				<Editor />
+			</div>
+		</LexicalComposer>
+	</SharedAutocompleteContext>
+</SettingsContext>
